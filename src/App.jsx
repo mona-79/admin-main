8fa1651a3a891d528adadfdfc527871e71033e31
mona-79 <mona736886@gmail.com>
2025-04-10 13:14:23 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Dashboard from './Pages/Dashboard';
+import Header from './Components/Header';
+import Sidebar from './Components/Sidebar';
+
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      exact:true,
+      element: (
+       <>
+       <section className='main'>
+        <Header/>
+        <div className='contentMain flex'>
+          <div className='sidebarWrapper w-[18%]'>
+            <Sidebar/>
+          </div>
+          </div>
+       </section>
+       </>
+      
+      ),
+    },
+  ]);
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
